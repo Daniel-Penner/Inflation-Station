ba8445e71d26c58ae3cd0a9b0b6f7125690c723e
Daniel Penner <danielwpenner@shaw.ca>
2024-03-21 16:23:22 -0700

    Update index.php

--- index.php
+++ index.php
@@ -12,7 +12,20 @@
 <?php
         if($_SERVER['REQUEST_METHOD']=='POST'){
         if (!empty($_POST['fname'])){
-        
+        try{
+            $connectionString = "mysql:host=localhost;dbname=db_54925359"; 
+            $username = "54925359";
+            $password = "54925359"; 
+    
+            // Create connection
+            $pdo = new PDO($connectionString, $username, $password);
+            
+            
+        }
+            catch(PDOException $e){
+                die($e->getMessage());
+                echo "return false;"
+              }
         }
     }
         ?>
