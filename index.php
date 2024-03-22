86bd6b23ef2269ebacbf896a19a952badd79ad73
Daniel Penner <danielwpenner@shaw.ca>
2024-03-21 20:35:29 -0700

    sessional changes

--- index.php
+++ index.php
@@ -1,3 +1,6 @@
+<?php
+session_start();
+?>
 <!DOCTYPE html>
 <html>
 
@@ -10,7 +13,6 @@
 </head>
 <body>
 <?php
-        session_start();
         if($_SERVER['REQUEST_METHOD']=='POST'){
         if (!empty($_POST['fname'])){
         try{
