3715f882216484067c804e0c74376bf64e1ee8ae
Daniel Penner <danielwpenner@shaw.ca>
2024-03-21 19:47:08 -0700

    Update login.php

--- login.php
+++ login.php
@@ -10,6 +10,44 @@
 </head>
 
 <body>
+    <?php
+        if($_SERVER['REQUEST_METHOD']=='POST'){
+            if (!empty($_POST['email'])){
+            try{
+                $connectionString = "mysql:host=localhost;dbname=db_54925359"; 
+                $username = "54925359";
+                $password = "54925359"; 
+        
+                // Create connection
+                $pdo = new PDO($connectionString, $username, $password);
+                $existingEmail = false;
+                $fileContent=file_get_contents($_POST['pfp']);
+                $sql = "SELECT userId, email, password FROM customer";
+                $statement = $pdo->prepare($sql);
+                $statement->execute();
+                $emailMatch = false;
+                while($row = $statement->fetch())
+                  {
+                    if($_POST['email'] == $row['email']){
+                        $emailMatch == true;
+                        if($_POST['password']==$row['password']{
+                            $_SESSION['id'] = $row['userId'];
+                        })
+                        else{
+                            echo "<script>alert('Unable to Log in: Email and Password do not match.')</script>";
+                        }
+                    }
+                    if (!$emailMatch){
+                        echo "<script>alert('Unable to Log in: Email and Password do not match.')</script>";
+                    }
+                  }
+        }
+                catch(PDOException $e){
+                    die($e->getMessage());
+                  }
+            }
+        }        
+    ?>
     <header>
         <div class="container">
             <div class="row justify-content-center">
@@ -42,7 +80,7 @@
             <h5 style="text-align:center; color:white; font-size:50px;">Login</h5>
         </div>
     </header>
-    <form method="GET" id="loginForm">
+    <form method="POST" action="login.php" id="loginForm">
         <fieldset>
             <br>
             <div class="row justify-content-center mx-auto">
@@ -50,7 +88,7 @@
                     <label for="email">
                         <p style="font-size:25px;"><i>Email</i></p>
                     </label><br>
-                    <input type="email" class="form-control" id="email" placeholder="Enter email" required><br>
+                    <input type="email" name="email" class="form-control" id="email" placeholder="Enter email" required><br>
                 </div>
             </div>
             <div class="row justify-content-center mx-auto">
@@ -58,7 +96,7 @@
                     <label for="password">
                         <p style="font-size:25px;"><i>Password</i></p>
                     </label><br>
-                    <input type="password" class="form-control" id="pass" placeholder="Password" required><br>
+                    <input type="password" name ="password" class="form-control" id="pass" placeholder="Password" required><br>
                 </div>
             </div>
         </fieldset>
@@ -67,7 +105,7 @@
     <div class="container">
         <div class="row">
             <div class="d-grid gap-0 col-3 mx-auto">
-                <button class="btn btn-outline-success btn-block" id="loginButton">Login</button>
+                <button type ="submit" class="btn btn-outline-success btn-block" id="loginButton">Login</button>
             </div>
             <div class="d-grid gap-0 col-3 mx-auto">
                 <button onclick="formReset()" class="btn btn-outline-warning btn-block" id="loginButton">Clear Entry</button>
@@ -92,8 +130,6 @@
             alert("Unable to Log in: Email field cannot be left blank.");
         } else if (pass === "") {
             alert("Unable to Log in: Password field cannot be left blank.");
-        } else {
-            
         }
     });
     function formReset() {
