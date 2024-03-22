04710c9faf7ffcbbce29120284c4e3e2582711ee
Daniel Penner <danielwpenner@shaw.ca>
2024-03-22 14:29:07 -0700

    Update login.php

--- login.php
+++ login.php
@@ -34,7 +34,7 @@ session_start();
                     if($_POST['email'] == $row['email']){
                         $emailMatch = true;
                         if($_POST['password']==$row['password']){
-                            echo '<img src="data:image/jpeg;base64,'.base64_encode($row['profilePicture']->load()).'"/>';
+                            echo '<img src="data:image/jpeg;base64,'.base64_encode($row['profilePicture']).'"/>';
                             $_SESSION['id'] = $row['customerId'];
                             $_SESSION['fname'] = $row['fname'];
                         }
