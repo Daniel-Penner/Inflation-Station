4a52293683b02cd68dff254bdaad25ce98c91c53
Daniel Penner <danielwpenner@shaw.ca>
2024-03-22 20:18:17 -0700

    change

--- products.php
+++ products.php
@@ -4,7 +4,6 @@ session_start();
 <!DOCTYPE html>
 <html lang="en">
 <meta charset="UTF-8">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
 
 <head>
     <title>Product Gallery - Inflation Station</title>
@@ -15,86 +14,84 @@ session_start();
 </head>
 
 <body>
-<div class="container">
-  <div class="row">
-    <div class="col">
-      <div class="card-deck">
-        <div class="card mb-3">
-          <img src="https://via.placeholder.com/350x150" class="card-img-top" alt="" />
-          <div class="card-body">
-            <h3 class="card-title">
-              card 1
-            </h3>
-            <div class="card-text">
-              Lorem ipsum lala longer text to see the equal height alignment. Hence you should not need h-100 or cols for a card-deck.
+    <header>
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-md-8">
+                    <div class="navborder">
+                        <nav class="navbar navbar-expand-lg navbar-light bg-light navround">
+                            <div class="container-fluid">
+                                <div class="navbar-nav">
+                                <?php 
+                                    if(isset($_SESSION['id'])) {
+                                    echo '<a class="navbar-brand" href="profile.php">
+                                        <img src="data:image/jpeg;base64,'.base64_encode($_SESSION['pfp']).'" alt="" width="30" height="30" style="border: 1px black solid; border-radius: 50%;">
+                                    </a>';
+                                    }
+                                    ?>
+                                    <a class="nav-link" href="index.php">Home</a>
+                                    <a class="nav-link" href="about.php">About</a>
+                                    <a class="nav-link" href="contact.php">Contact</a>
+                                    <a class="nav-link" href="products.php">Products</a>
+                                    <a class="nav-link" href="search.php">Explore</a>
+                                </div>
+                                <form class="d-flex" action="products.php" method ="get">
+                                <input class="form-control me-2" type="search" name ="search" placeholder="Search" <?php if(!empty($_GET['search'])){echo "value = " . $_GET['search'] . " ";}?>
+                                        aria-label="Search">
+                                    <button class="btn btn-outline-success" type="submit">Search</button>
+                                </form>
+                            </div>
+                        </nav>
+                    </div>
+                </div>
             </div>
-          </div>
-          <a class="card-footer btn btn-primary bg-primary" href="/shows/card-1"> Explore </a>
         </div>
-        <div class="card mb-3">
-          <img src="https://via.placeholder.com/350x150" class="card-img-top" alt="" />
-          <div class="card-body">
-            <h3 class="card-title">
-              card 2
-            </h3>
-            <div class="card-text">
-              Lorem ipsum lala
-            </div>
-          </div>
-          <a class="card-footer btn btn-primary bg-primary" href="/shows/card-2"> Explore </a>
+        <div class=homehb>
+            <h5 style="text-align:center; color:white; font-size:50px;">Products</h5>
         </div>
-        <div class="card mb-3">
-          <img src="https://via.placeholder.com/350x150" class="card-img-top" alt="" />
-          <div class="card-body">
-            <h3 class="card-title">
-              card 3
-            </h3>
-            <div class="card-text">
-              Lorem ipsum lala
-            </div>
-          </div>
-          <a class="card-footer btn btn-primary bg-primary" href="/shows/card-3"> Explore </a>
+    </header>
+    <div class="container">
+    <div class="row">
+        <div class="col">
+            <div class="card-deck">
+            <?php
+            // Database connection
+            try{
+            $connectionString = "mysql:host=localhost;dbname=db_54925359"; 
+            $username = "54925359";
+            $password = "54925359"; 
+    
+            // Create connection
+            $pdo = new PDO($connectionString, $username, $password);
+
+    
+            // SQL query to fetch posts data with author information
+            
+            $searchFor = '%' . $_GET['search'] . '%';
+            $sql = "SELECT * FROM product WHERE productName LIKE ?";
+            $statement = $pdo->prepare($sql);
+            $statement->bindValue(1, $searchFor);
+            $statement->execute();
+            while($row = $statement->fetch())
+            {
+                  echo "<div class='card mb-3'";
+                      echo "<img src=" . $row['productImageURL'] . " class='card-img-top' alt=''>";
+                        echo "<div class='card-body'>";
+                          echo "<h5 class='card-title'>" . $row['productName'] . "</h5>";
+                          echo "<p class='card-text'>$" . $row['productPrice'] ."/lb</p>";
+                          echo "<a href='indvproduct.php?prod=" . $row['productId'] ."' class='button'>More Info</a>";
+                        echo "</div>";
+                      echo "</div>";
+              }
+            }
+            catch(PDOException $e){
+              die($e->getMessage());
+            }
+            ?>
         </div>
-        <div class="card mb-3">
-          <img src="https://via.placeholder.com/350x150" class="card-img-top" alt="" />
-          <div class="card-body">
-            <h3 class="card-title">
-              card 4
-            </h3>
-            <div class="card-text">
-              Lorem ipsum lala
-            </div>
-          </div>
-          <a class="card-footer btn btn-primary bg-primary" href="/shows/card-4"> Explore </a>
         </div>
-        <div class="card mb-3">
-          <img src="https://via.placeholder.com/350x150" class="card-img-top" alt="" />
-          <div class="card-body">
-            <h3 class="card-title">
-              card 5
-            </h3>
-            <div class="card-text">
-              Lorem ipsum lala
-            </div>
-          </div>
-          <a class="card-footer btn btn-primary bg-primary" href="/shows/card-5"> Explore </a>
-        </div>
-        <div class="card mb-3">
-          <img src="https://via.placeholder.com/350x150" class="card-img-top" alt="" />
-          <div class="card-body">
-            <h3 class="card-title">
-              card 6
-            </h3>
-            <div class="card-text">
-              Lorem ipsum lala
-            </div>
-          </div>
-          <a class="card-footer btn btn-primary bg-primary" href="/shows/card-6"> Explore </a>
-        </div>
-      </div>
     </div>
-  </div>
-</div>
+    </div>
 </body>
 
 </html>
