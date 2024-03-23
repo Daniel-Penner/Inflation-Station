b30dea08934c5ddb6d59e59580987eb3de6a58d5
Daniel Penner <danielwpenner@shaw.ca>
2024-03-22 20:12:32 -0700

    Update products.php

--- products.php
+++ products.php
@@ -75,14 +75,18 @@ session_start();
             $statement->execute();
             while($row = $statement->fetch())
             {
-                  echo "<div class='card mb-3'";
-                      echo "<img src=" . $row['productImageURL'] . " class='card-img-top' alt=''>";
-                        echo "<div class='card-body'>";
-                          echo "<h5 class='card-title'>" . $row['productName'] . "</h5>";
-                          echo "<p class='card-text'>$" . $row['productPrice'] ."/lb</p>";
-                          echo "<a href='indvproduct.php?prod=" . $row['productId'] ."' class='button'>More Info</a>";
-                        echo "</div>";
-                      echo "</div>";
+                  echo '<div class="card mb-3">
+                  <img src="https://via.placeholder.com/350x150" class="card-img-top" alt="" />
+                  <div class="card-body">
+                    <h3 class="card-title">
+                      card 1
+                    </h3>
+                    <div class="card-text">
+                      Lorem ipsum lala longer text to see the equal height alignment. Hence you should not need h-100 or cols for a card-deck.
+                    </div>
+                  </div>
+                  <a class="card-footer btn btn-primary bg-primary" href="/shows/card-1"> Explore </a>
+                </div>';
               }
             }
             catch(PDOException $e){
