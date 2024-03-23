bcb44d657bf1b152a773f084c60e4f3a8cc3a92c
Daniel Penner <danielwpenner@shaw.ca>
2024-03-22 20:08:56 -0700

    Update products.php

--- products.php
+++ products.php
@@ -75,7 +75,7 @@ session_start();
             $statement->execute();
             while($row = $statement->fetch())
             {
-                  echo "<div class='card mb-3'";
+                  echo "<div class='card mb-3' style='width: 18rem;";
                       echo "<img src=" . $row['productImageURL'] . " class='card-img-top'>";
                         echo "<div class='card-body'>";
                           echo "<h5 class='card-title'>" . $row['productName'] . "</h5>";
