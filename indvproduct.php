048875c4bc3271d8ae11d58c513eb75883f716e2
Mark <mark.lovesey03@gmail.com>
2024-03-23 20:41:17 -0700

    fixed styling, added submit refusal.

--- indvproduct.php
+++ indvproduct.php
@@ -15,8 +15,9 @@ session_start();
 
 <body>
     <?php
-    
+
     // Database connection
+    /*
     try{
         $connectionString = "mysql:host=localhost;dbname=db_54925359"; 
         $username = "54925359";
@@ -24,7 +25,6 @@ session_start();
 
         // Create connection
         $pdo = new PDO($connectionString, $username, $password);
-
 
         // SQL query to fetch posts data with author information
         
@@ -42,22 +42,22 @@ session_start();
         }
         catch(PDOException $e){
           die($e->getMessage());
-        }
+        }*/
     ?>
     <header>
         <div class="container">
-            <div class="row">
+            <div class="row justify-content-center">
                 <div class="col-md-8">
                     <div class="navborder">
                         <nav class="navbar navbar-expand-lg navbar-light bg-light navround">
                             <div class="container-fluid">
                                 <div class="navbar-nav">
-                                    <?php 
-           if(isset($_SESSION['id'])) {
-           echo '<a class="navbar-brand" href="profile.php">
-               <img src="data:image/jpeg;base64,'.base64_encode($_SESSION['pfp']).'" alt="" width="30" height="30" style="border: 1px black solid; border-radius: 50%;">
-           </a>';
-           }
+                                    <?php /*
+                if(isset($_SESSION['id'])) {
+                echo '<a class="navbar-brand" href="profile.php">
+                    <img src="data:image/jpeg;base64,'.base64_encode($_SESSION['pfp']).'" alt="" width="30" height="30" style="border: 1px black solid; border-radius: 50%;">
+                </a>';
+                }*/
                                     ?>
                                     <a class="nav-link" href="index.php">Home</a>
                                     <a class="nav-link" href="about.php">About</a>
@@ -78,27 +78,24 @@ session_start();
         </div>
         <div class=homehb>
             <h5 style="text-align:center; color:white; font-size:50px;">
-                <?php echo $name;     ?>
+                <?php //echo $name;          ?>
             </h5>
         </div>
     </header>
-
-
-
     <br>
     <div class="container justify-content-center">
         <div class="row">
-            <div class="col-sm-8"><img src="<?php echo $image;     ?>" class="productimage"></div>
+            <div class="col-sm-8"><img src="<?php //echo $image;          ?>" class="productimage"></div>
             <div class="col-sm-4">
                 <div class="sidecol">
                     <p style="font-size:25px; color:white;"><strong>Information</strong></p>
                     <hr style="color:white; height:8px;" />
                     <div class=incol>
                         <li>Price: $
-                            <?php echo $price;     ?>/lb
+                            <?php //echo $price;          ?>/lb
                         </li>
                         <li>Description:
-                            <?php echo $desc;     ?>
+                            <?php //echo $desc;          ?>
                         </li>
                     </div>
                     <br>
@@ -130,44 +127,77 @@ session_start();
             <p style="font-size:25px; color:white;"><strong>Reviews</strong></p>
             <hr style="color:white; height:8px;" />
             <br>
+            <!--Create Comment Start-->
             <div class="card"
                 style="background-color: rgb(182,212,189); max-width: 50rem; border-radius: 1rem; padding: 1rem;">
                 <div class="card-body">
-                    <h5 class="card-title" style="color:white;">Leave a Comment</h5>
+                    <h5 class="card-title" style="color:white; font-size:1.5em;"><strong>Leave a Comment</strong></h5>
+                    <div class="row">
+                        <!-- User Image -->
+                    </div>
                     <form>
-                        <div class="form-group" style="max-width: 10rem; margin: 0 auto;">
-                            <label for="rating" style="color: white; padding: 0.5rem;">Rating</label>
-                            <select class="form-control" id="rating">
-                                <option value="1">1</option>
-                                <option value="2">2</option>
-                                <option value="3">3</option>
-                                <option value="4">4</option>
-                                <option value="5">5</option>
-                            </select>
-                        </div>
+                        <!-- Rating -->
                         <div class="form-group">
-                            <label for="message" style="color: white; padding: 0.5rem;">Message</label>
-                            <textarea class="form-control" id="message" rows="3"
-                                placeholder="Enter your message"></textarea>
+                            <div class="col">
+                                <label for="rating"
+                                    style="color: white; padding: 0.4rem; font-size:1.3em;"><strong>Rating</strong></label>
+                                <select class="form-control" id="rating"
+                                    style="max-width:5em; margin: 0 auto; text-align:center;">
+                                    <option value="1">1 Star</option>
+                                    <option value="2">2 Stars</option>
+                                    <option value="3">3 Stars</option>
+                                    <option value="4">4 Stars</option>
+                                    <option value="5">5 Stars</option>
+                                </select>
+                            </div>
                         </div>
+                        <!-- Message -->
+                        <label for="message"
+                            style="color: white; padding: 0.4rem; font-size:1.3em; text-align:left;"><strong>Message</strong></label>
+                        <textarea class="form-control" id="message" rows="3"
+                            placeholder="Enter your message"></textarea>
                         <br>
-                        <button type="submit" class="btn btn-success">Submit</button>
+                        <!-- Submit Button -->
+                        <?php
+                        if (isset ($_SESSION['id'])) {
+                            echo "<button type='submit' class='btn btn-success btn-lg'>Submit</button>";
+                        } else {
+                            echo "<button type='submit' class='btn btn-success btn-lg' disabled>Submit</button>";
+                            echo "<br>";
+                            echo "<p style='color:red;'>You must be <a href='login.php'>logged in</a> to comment.</p>";
+                        }
+                        ?>
+
                     </form>
                 </div>
             </div>
+            <!--Create Comment End-->
         </div>
         <br>
+
         <div class="row justify-content-center"
             style="max-width:50rem; background-color:rgb(182,212,189); margin: 0 auto; border-radius: 1rem; padding: 1rem; overflow-y: auto;">
+            
+            <!--Comment-->
             <div class="row " style="background-color:white; border-radius: 1rem; padding: 1rem;">
-            <div class="col-auto">
-                <img src="images\apple.jpg" alt="Profile Picture"
-                    style="width:5rem; border-radius: 5rem; padding 4rem;">
-                <span>John Doe</span></div>
-                <span style="position: relative; text-align: right;">✰✰✰✰✰</span>
-                <p style="position: relative; text-align:left;">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
+                <div class="col-auto">
+                    <?php
+                    if (isset ($_SESSION['id'])) {
+                        echo '<a class="navbar-brand" href="profile.php">
+                    <img src="data:image/jpeg;base64,' . base64_encode($_SESSION['pfp']) . '" alt="" style="border: 1px black solid; width:5rem; border-radius: 5rem; padding 4rem;">
+                    </a>';
+                    }
+                    ?>
+                    <span>
+                        <?php echo $_SESSION['fname'] + " " + $SESSION['lname']; ?>
+                    </span>
+                </div>
+                <span style="position: relative; text-align: right;">Rating: 3</span>
+                <p style="position: relative; text-align:left;">Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Sed
                     viverra dui eget nunc efficitur, nec pretium risus varius.</p>
             </div>
+            <!--End Comment-->
         </div>
     </div>
 </body>
