a5f9d3f0b960e072c6ad151522f58dfd0f2183c4
Mark <mark.lovesey03@gmail.com>
2024-03-26 17:34:29 -0700

    admin navigation

--- admindashboard.php
+++ admindashboard.php
@@ -42,7 +42,9 @@ session_start();
                                     <a class="nav-link" href="products.php">Products</a>
                                     <a class="nav-link" href="search.php">Explore</a>
                                     <?php
-                                    if(isset($_SESSION['type']))
+                                    if(isset($_SESSION['type'])) {
+                                        echo '<a class="nav-link" href="admindashboard.php">Admin</a>';
+                                    }
                                     ?>
                                 </div>
                                 <form class="d-flex" action="products.php" method="get">
