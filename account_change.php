fb25bdb3e50b2d37f8c4dcd1cf0393778c826550
Mark <mark.lovesey03@gmail.com>
2024-03-27 00:21:48 -0700

    admin change finished

--- account_change.php
+++ account_change.php
@@ -1,13 +1,14 @@
 <?php
+session_start();
 try {
     include 'dbconnection.php';
-    /*
+    
     if (isset($_SESSION['type'])) { //check if admin
 
     } else { // if user is not admin
         header("Location: index.php");
         exit();
-    }*/
+    }
     $changeUserId = 0;
     if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         if (!empty($_POST['uuid'])) { // if true the request is sent by admin dashboard
@@ -16,10 +17,10 @@ try {
         } elseif (isset($_SESSION['id'])) { // if true then the request is from a regular user changing their information
             $changeUserId = $_SESSION['id'];
         }
-       /* if(!empty($_POST['fname']) && !empty($_POST['lname']) && !empty($_POST['email']) && !empty($_POST['pass'])) {
+        if(!empty($_POST['fname']) && !empty($_POST['lname']) && !empty($_POST['email']) && !empty($_POST['pass'])) {
             header("Location: index.php"); // verify for any empty fields, if empty send them back to the homepage
             exit();
-        } */
+        } 
         $sql = "UPDATE customer 
         SET fname = ?, lname = ?, email = ?, password = ?
         WHERE customerId = ?";
