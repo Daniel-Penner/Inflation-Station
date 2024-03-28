7448c8da352c1e8369f23c6e4891757caa677b62
Daniel Penner <danielwpenner@shaw.ca>
2024-03-27 20:07:51 -0700

    Update index.php

--- index.php
+++ index.php
@@ -177,7 +177,7 @@ function validateLastName($lname) {
                     $statement->execute();
                 while ($row = $statement->fetch()) {
                         echo $row['productId'];
-                        echo '<img src="' . $row["productImageURL"] . '" width = "50px"/>';
+                        echo '<img src="' . $row["productImageURL"] . '"/>';
                     }
                 } catch (PDOException $e) {
                     die($e->getMessage());
