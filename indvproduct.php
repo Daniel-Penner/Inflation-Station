e9c63fd669026e8a5f1aa57e874e863ca2142b85
Daniel Penner <danielwpenner@shaw.ca>
2024-03-27 19:04:51 -0700

    Update indvproduct.php

--- indvproduct.php
+++ indvproduct.php
@@ -203,7 +203,7 @@ date_default_timezone_set('Canada/Pacific');
             method: 'POST',
             data: { commentId: commentId },
             success: function(response) {
-                this.closest('.row').delete()
+                this.delete()
                 // on comment deletion, remove comment from DOM
                 if (response === 'success') {
                     console.log('Comment deleted successfully');
