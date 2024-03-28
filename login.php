61fb9c336b9b46d6b6d301f7e3fb8278bf65efe3
Daniel Penner <danielwpenner@shaw.ca>
2024-03-28 13:34:59 -0700

    Update login.php

--- login.php
+++ login.php
@@ -44,13 +44,12 @@ function validatePassword($password) {
                 //If user is banned (0 -> not banned 1 -> banned)
                 $emailMatch = false;
                 while ($row = $statement->fetch(PDO::FETCH_ASSOC)) {
+                    if ($row['isBanned'] == "true") {
+                        echo "<script>alert('Your account is currently banned from Inflation Station, please contact an administrator for further details.')</script>";
+                        header('Location: index.php');
+                        exit();
+                    }
                     if ($_POST['email'] == $row['email']) {
-                        if ($row['isBanned'] == "true") {
-                            echo "<script>alert('Your account is currently banned from Inflation Station, please contact an administrator for further details.');</script>";
-                            header('Location: index.php');
-                            exit();
-                        }
-                        else{
                         $emailMatch = true;
                         if (md5($_POST['password']) == $row['password']) {
                             $_SESSION['pfp'] = $row['profilePicture'];
@@ -61,7 +60,6 @@ function validatePassword($password) {
                             $_SESSION['fname'] = $row['fname'];
                             $_SESSION['lname'] = $row['lname'];
                             $_SESSION['isBanned'] = $row['isBanned'];
-                        }
                         } else {
                             echo "<script>alert('Unable to Log in: Email and Password do not match.')</script>";
                         }
