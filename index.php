9a1ad43910c0bc0397880c45ee912375c0be36a7
Daniel Penner <danielwpenner@shaw.ca>
2024-03-27 20:01:56 -0700

    Update index.php

--- index.php
+++ index.php
@@ -166,9 +166,14 @@ function validateLastName($lname) {
                 <?php
                 // get products for home page
                 try {
-                    include 'dbonnection.php';
+                    $connectionString = "mysql:host=localhost;dbname=db_54925359";
+    $username = "54925359";
+    $password = "54925359";
+
+    // Create connection
+    $pdo = new PDO($connectionString, $username, $password);
                     $sql = "SELECT * FROM product LIMIT 4;";
-                    //$statement = $pdo->prepare($sql);
+                    $statement = $pdo->prepare($sql);
                     //$statement->execute();
                     //while ($row = $statement->fetch()) {
                         //echo $row['productId'];
